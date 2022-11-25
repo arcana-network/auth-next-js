8967028b5f9ea2ba45b3cf836e351bdd3c6c1bba
makylfang <makyl@newfang.io>
2022-11-25 14:57:03 +0400

    Updated arcana auth version, added hook example for using auth

--- auth/init.js
+++ auth/init.js
@@ -1,10 +1,72 @@
 import { AuthProvider } from "@arcana/auth";
+import React from "react";
+const auth = new AuthProvider(process.env.NEXT_PUBLIC_ARCANA_APP_ID);
 
-let auth = new AuthProvider(process.env.NEXT_PUBLIC_ARCANA_APP_ID);
+const useArcanaAuth = () => {
+  const [loading, setLoading] = React.useState(true);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const [availableLogins, setAvailableLogins] = React.useState([]);
+  const [user, setUser] = React.useState(null);
+  const providerRef = React.useRef(null);
 
-const InitializeAuth = async () => {
-  await auth.init();
-  return auth;
+  const loginWithSocial = async (p) => {
+    await auth.init();
+    await auth.loginWithSocial(p);
+  };
+
+  const loginWithLink = async (email) => {
+    await auth.init();
+    return auth.loginWithLink(email);
+  };
+
+  const logout = async () => {
+    if (await auth.isLoggedIn()) {
+      await auth.logout();
+    }
+  };
+
+  const onConnectHook = async () => {
+    setIsLoggedIn(true);
+    const info = await auth.getUser();
+    setUser(info);
+  };
+
+  const onDisconnectHook = () => {
+    setIsLoggedIn(false);
+  };
+
+  React.useEffect(() => {
+    auth.provider.on("connect", onConnectHook);
+    auth.provider.on("disconnect", onDisconnectHook);
+    auth.init().then(() => {
+      setLoading(false);
+      auth.isLoggedIn().then((loggedIn) => {
+        if (!loggedIn) {
+          auth.getLogins().then((logins) => {
+            console.log({ logins });
+            setAvailableLogins(logins.filter((l) => l != "passwordless"));
+          });
+        }
+        setIsLoggedIn(loggedIn);
+      });
+    });
+    return () => {
+      auth.provider.removeListener("connect", onConnectHook);
+      auth.provider.removeListener("disconnect", onDisconnectHook);
+    };
+  }, []);
+
+  return {
+    availableLogins,
+    loading,
+    loginWithLink,
+    loginWithSocial,
+    logout,
+    provider: auth.provider,
+    isLoggedIn,
+    user,
+    appId: auth.appId,
+  };
 };
 
-export { InitializeAuth };
+export { useArcanaAuth };
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,15 @@
-import { useEffect, useState, useRef } from "react";
-import { InitializeAuth } from "../auth/init";
+import { useArcanaAuth } from "../auth/init";
 
 export default function IndexPage() {
-  const authRef = useRef(null);
-  const [loaded, setLoaded] = useState(false);
-  const [loggedIn, setLoggedIn] = useState(false);
-  useEffect(() => {
-    InitializeAuth().then(async (auth) => {
-      setLoaded(true);
-      authRef.current = auth;
-      const isLoggedIn = await authRef.current.isLoggedIn();
-      setLoggedIn(isLoggedIn);
-    });
-  }, []);
+  const { isLoggedIn, loading, loginWithSocial, provider } = useArcanaAuth();
   return (
     <>
-      {!loaded && "Loading..."}
-      {loaded && (
+      {loading && "Loading..."}
+      {!loading && (
         <div>
-          IsLoggedIn: {loggedIn ? "Yes" : "No"}
-          {!loggedIn && (
-            <button onClick={() => authRef.current?.loginWithSocial("google")}>
+          IsLoggedIn: {isLoggedIn ? "Yes" : "No"}
+          {!isLoggedIn && (
+            <button onClick={() => loginWithSocial("google")}>
               Login With Google
             </button>
           )}
